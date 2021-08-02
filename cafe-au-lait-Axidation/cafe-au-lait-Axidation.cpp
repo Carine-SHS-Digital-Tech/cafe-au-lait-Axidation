7bc6ccc14e95c1644001897d8cd1527b98cf124f
Callan <43071916+Axidation@users.noreply.github.com>
2021-08-02 12:29:40 +0800

    Additional tests

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -6,7 +6,13 @@ int main()
     string choice1;
     std::cout << "Daily Summary(1) or New order(2)?\n";
     std::cin >> choice1;
-    std::cout << choice1;
+
+    if (choice1 == "1")
+    {
+
+        std::cout << "Gaming\n";
+
+    };
 
     return 0;
 }
