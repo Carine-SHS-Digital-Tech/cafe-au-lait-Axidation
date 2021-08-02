b6d1ef0f714fca492a390e4e9e3266c8a358b6e6
Callan <43071916+Axidation@users.noreply.github.com>
2021-08-02 12:19:32 +0800

    Question 1 test

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string choice1;
+    std::cout << "Daily Summary(1) or New order(2)?\n";
+    std::cin >> choice1;
+    std::cout << choice1;
+
+    return 0;
 }
