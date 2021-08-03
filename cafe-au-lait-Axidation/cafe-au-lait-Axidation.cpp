209091d95e489f86c579e77cc2483bb4abf9e2f3
Callan <43071916+Axidation@users.noreply.github.com>
2021-08-03 10:00:28 +0800

    Started NewOrder code

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void NewOrder()
 {
+    float Cappuccino = 0;
+    float Espresso = 0;
+    float Latte = 0;
+    float IcedCoffee = 0;
+
+
+    string choice2;
+    std::cout << "Dine-In(1) or Take-Away(2)?\n";
+    std::cin >> choice2;
+
+    if (choice2 == "1");
+    {
+
+        Cappuccino = 3;
+        Espresso = 2.25;
+        Latte = 2.5;
+        IcedCoffee = 2.5;
+
+    }
+
+    printf("~-MENU-~\nCappuccino $%.2f\nEsspresso $%.2f\nLatte $%.2f\nIced Coffee $%.2f\n", Cappuccino, Espresso, Latte, IcedCoffee);
+
+}
+
+void program()
+{
+
     string choice1;
     std::cout << "Daily Summary(1) or New order(2)?\n";
     std::cin >> choice1;
 
-    if (choice1 == "1")
-    {
 
-        std::cout << "Gaming\n";
 
-    };
+    //if (choice1 == "1") DailySummary();
+    if (choice1 == "2") NewOrder();
 
-    return 0;
-}
+    
+};
+
+int main()
+{
+    program();
+};
