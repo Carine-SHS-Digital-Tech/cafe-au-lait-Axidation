7263e99a7e44150d95c976696ace866b8742c1b9
Callan <43071916+Axidation@users.noreply.github.com>
2021-08-04 12:29:20 +0800

    Additional New Order code

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -7,23 +7,36 @@ void NewOrder()
     float Espresso = 0;
     float Latte = 0;
     float IcedCoffee = 0;
+    string ch2surcharge = "";
 
 
     string choice2;
     std::cout << "Dine-In(1) or Take-Away(2)?\n";
+    std::cout << "*Take-Away incurs a 5% surcharge\n";
     std::cin >> choice2;
 
-    if (choice2 == "1");
+    if (choice2 == "1")
     {
 
-        Cappuccino = 3;
-        Espresso = 2.25;
-        Latte = 2.5;
-        IcedCoffee = 2.5;
+        Cappuccino = 3.3f;
+        Espresso = 2.475f;
+        Latte = 2.75f;
+        IcedCoffee = 2.75f;
 
     }
 
-    printf("~-MENU-~\nCappuccino $%.2f\nEsspresso $%.2f\nLatte $%.2f\nIced Coffee $%.2f\n", Cappuccino, Espresso, Latte, IcedCoffee);
+    if (choice2 == "2")
+    {
+
+        Cappuccino = 3.465;
+        Espresso = 2.59875;
+        Latte = 2.8875;
+        IcedCoffee = 2.8875;
+        ch2surcharge = " + 5 % Surcharge";
+
+    }
+
+    printf("~-MENU-~ (inc. 10% GST%s)\nCappuccino $%.2f\nEsspresso $%.2f\nLatte $%.2f\nIced Coffee $%.2f\n", ch2surcharge, Cappuccino, Espresso, Latte, IcedCoffee);
 
 }
 
