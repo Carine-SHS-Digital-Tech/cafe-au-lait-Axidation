daeea61ca18ce765695e0763e9a735828db2f0b7
Callan <43071916+Axidation@users.noreply.github.com>
2021-08-05 11:01:33 +0800

    Fixed menu and added order input

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -7,13 +7,16 @@ void NewOrder()
     float Espresso = 0;
     float Latte = 0;
     float IcedCoffee = 0;
-    string ch2surcharge = "";
-
+    string Item1;
+    string Item2;
+    string Item3;
+    string Item4;
 
     string choice2;
     std::cout << "Dine-In(1) or Take-Away(2)?\n";
     std::cout << "*Take-Away incurs a 5% surcharge\n";
     std::cin >> choice2;
+    std::cout << "\n";
 
     if (choice2 == "1")
     {
@@ -22,6 +25,7 @@ void NewOrder()
         Espresso = 2.475f;
         Latte = 2.75f;
         IcedCoffee = 2.75f;
+        printf("~-MENU-~ (inc. 10%% GST)\n1 - Cappuccino $%.2f\n2 - Esspresso $%.2f\n3 - Latte $%.2f\n4 - Iced Coffee $%.2f\n5 - None\n\n", Cappuccino, Espresso, Latte, IcedCoffee);
 
     }
 
@@ -32,11 +36,15 @@ void NewOrder()
         Espresso = 2.59875;
         Latte = 2.8875;
         IcedCoffee = 2.8875;
-        ch2surcharge = " + 5 % Surcharge";
+        printf("~-MENU-~ (inc. 10%% GST + 5%% Surcharge)\n1 - Cappuccino $%.2f\n2 - Esspresso $%.2f\n3 - Latte $%.2f\n4 - Iced Coffee $%.2f\n5 - None\n\n", Cappuccino, Espresso, Latte, IcedCoffee);
 
     }
 
-    printf("~-MENU-~ (inc. 10% GST%s)\nCappuccino $%.2f\nEsspresso $%.2f\nLatte $%.2f\nIced Coffee $%.2f\n", ch2surcharge, Cappuccino, Espresso, Latte, IcedCoffee);
+    printf("Please enter desired items (Up to 4)\n");
+    printf("1: "); std::cin >> Item1;
+    printf("2: "); std::cin >> Item2;
+    printf("3: "); std::cin >> Item3;
+    printf("4: "); std::cin >> Item4;
 
 }
 
@@ -44,18 +52,30 @@ void program()
 {
 
     string choice1;
-    std::cout << "Daily Summary(1) or New order(2)?\n";
+    std::cout << "New order(1) or Daily Summary(2)?\n";
     std::cin >> choice1;
+    std::cout << "\n";
 
 
-
+    if (choice1 == "1") NewOrder();
     //if (choice1 == "1") DailySummary();
-    if (choice1 == "2") NewOrder();
 
-    
 };
 
 int main()
 {
-    program();
+
+    string restart;
+
+    while (restart != "N")
+    {
+
+        program();
+
+        printf("\nRestart program? (Y/N)\n");
+        cin >> restart;
+        printf("\n");
+
+    }
+
 };
