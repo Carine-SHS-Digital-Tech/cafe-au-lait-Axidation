184ea4ad22283ca64bb00f3e2f1b9b7b539fa4a7
CallanIsTaken <zoomatron10@gmail.com>
2021-08-08 22:20:31 +0800

    Added comments

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 using namespace std;
 
+//Global variables
 int T_DineIn = 0;
 int T_TakeOut = 0;
 int T_Order = 0;
@@ -14,10 +15,13 @@ int T_Cups = 0;
 float T_TotalGST = 0;
 float T_TotalPrice = 0;
 
+//Create the summary csv file
 ofstream DailySummaryCSV("DailySummary.csv");
 
+//Code that runs when "New Order" is selected
 void NewOrder()
 {
+    //Local variables
     int DineIn = 0;
     int TakeOut = 0;
     int NumCappuccino = 0;
@@ -32,7 +36,10 @@ void NewOrder()
     float Latte;
     float IcedCoffee;
     float Surcharge = 0;
+    float Tendered;
+    float Change;
 
+    //Determines whether or not surcharge is applied
     string choice2;
     cout << "Dine-In(1) or Take-Away(2)?\n";
     cout << "*Take-Away incurs a 5% surcharge\n";
@@ -40,7 +47,7 @@ void NewOrder()
     if (choice2 != "1" && choice2 != "2") { printf("\nAn error occured."); return; }
     cout << "\n";
 
-    if (choice2 == "1")
+    if (choice2 == "1") //Surcharge not applied
     {
 
         Cappuccino = 3.3f;
@@ -52,7 +59,7 @@ void NewOrder()
 
     }
 
-    if (choice2 == "2")
+    if (choice2 == "2") //Surcharge applied
     {
 
         Cappuccino = 3.465;
@@ -64,29 +71,39 @@ void NewOrder()
 
     }
 
+    //Order input
     printf("Please enter desired amount per item\n");
     printf("1: "); cin >> NumCappuccino;
+    if (NumCappuccino < 0) { printf("\nAn error occured."); return; };
     printf("2: "); cin >> NumEspresso;
+    if (NumCappuccino < 0) { printf("\nAn error occured."); return; };
     printf("3: "); cin >> NumLatte;
+    if (NumCappuccino < 0) { printf("\nAn error occured."); return; };
     printf("4: "); cin >> NumIcedCoffee;
+    if (NumCappuccino < 0) { printf("\nAn error occured."); return; };
     cout << "\n";
 
+    //Calculates surcharge if applicable
     if (choice2 == "2") Surcharge = 0.165 * NumCappuccino + 0.12375 * NumEspresso + 0.1375 * NumLatte + 0.1375 * NumIcedCoffee;
 
+    //Calculates total order price
     TotalPrice = NumCappuccino * Cappuccino + NumEspresso * Espresso + NumLatte * Latte + NumIcedCoffee * IcedCoffee;
 
+    //Prints order summary
     printf("| Quantity | Item        | Single Item Ex. GST | GST         |  Total  |\n");
     printf("| %-9.d| Cappuccino  | $3.00               | $0.30       | $%-6.2f |\n", NumCappuccino, NumCappuccino * Cappuccino);
     printf("| %-9.d| Espresso    | $2.25               | $0.23       | $%-6.2f |\n", NumEspresso, NumEspresso * Espresso);
     printf("| %-9.d| Latte       | $2.50               | $0.25       | $%-6.2f |\n", NumLatte, NumLatte * Latte);
     printf("| %-9.d| Iced Coffee | $2.50               | $0.25       | $%-6.2f |\n\n", NumIcedCoffee, NumIcedCoffee * IcedCoffee);
     printf(" Surcharge : $%-6.2f Overall Total : $%6.2f \n\n", Surcharge, TotalPrice);
-
-    float Tendered;
-    float Change;
+    
+    //Input for amount tendered
     printf("Amount Tendered ($): "); cin >> Tendered; cout << "\n\n\n";
+
+    //Calculates Change
     Change = Tendered - TotalPrice;
 
+    //Prints sales receipt
     printf("SALES RECEIPT -\n");
     printf("_____---------------*****~~~| Cafe au Lait |~~~*****---------------_____\n");
     printf("| Quantity | Item        | Single Item Ex. GST | GST         |  Total  |\n");
@@ -97,9 +114,13 @@ void NewOrder()
     printf(" Surcharge : $%-6.2f Overall Total : $%-6.2f\n", Surcharge, TotalPrice);
     printf(" Tendered : $%-6.2f Change : $%-6.2f\n\n", Tendered, Change);
 
+    //Calculates total order GST
     TotalGST = 0.3 * NumCappuccino + 0.225 * NumEspresso + 0.25 * NumLatte + 0.25 * NumIcedCoffee;
+
+    //Calculates total items purchased
     Cups = NumCappuccino + NumEspresso + NumLatte + NumIcedCoffee;
 
+    //Updates global variables with new data
     T_DineIn += DineIn;
     T_TakeOut += TakeOut;
     T_Order += 1;
@@ -115,8 +136,10 @@ void NewOrder()
 
 };
 
+//Code that runs when "Daily Summary" is selected
 void DailySummary()
 {
+    //Prints summary to console
     printf("\nTotal Dine-In    |  %d", T_DineIn);
     printf("\nTotal Take-Away  |  %d", T_TakeOut);
     printf("\nTotal Orders     |  %d", T_Order);
@@ -128,10 +151,11 @@ void DailySummary()
     printf("\nTotal GST        | $%.2f", T_TotalGST);
     printf("\nTotal Income     | $%.2f\n\n", T_TotalPrice);
 
+    //Prints summary to csv file
     DailySummaryCSV << "Total Dine-In, " << T_DineIn;
     DailySummaryCSV << "\nTotal Take-Away, " << T_TakeOut;
     DailySummaryCSV << "\nTotal Orders, " << T_Order;
-    DailySummaryCSV << "\nTotal Cappuccino, " T_NumCappuccino;
+    DailySummaryCSV << "\nTotal Cappuccino, " << T_NumCappuccino;
     DailySummaryCSV << "\nTotal Espresso, " << T_NumEspresso;
     DailySummaryCSV << "\nTotal Latte, " << T_NumLatte;
     DailySummaryCSV << "\nTotal Iced Coffee, " << T_NumIcedCoffee;
@@ -145,26 +169,30 @@ void DailySummary()
 void main()
 {
 
+    //Local variables
     string restart;
     string choice1;
 
-    int Orders = 0; int DineIn = 0; int TakeOut = 0; int TotalCups = 0; float NumCappuccino = 0;
-    float NumEspresso = 0; float NumLatte = 0; float NumIcedCoffee = 0; float TotalGST = 0; float TotalPrice = 0;
-
+    //Allows code to run infinitely
     while (restart != "n")
     {
 
+        //Selection of function
         cout << "New order(1) or Daily Summary(2)?\n";
         cin >> choice1;
         cout << "\n";
 
         if (choice1 == "1") NewOrder();
         if (choice1 == "2") DailySummary();
+
+        //Error function if input invalid
         if (choice1 != "1" && choice1 != "2") { printf("\nAn error occured."); return; }
 
         printf("\nBack to Start? (y/n)\n");
         cin >> restart;
         printf("\n");
+
+        if (restart != "y") break;
 
     }
 
