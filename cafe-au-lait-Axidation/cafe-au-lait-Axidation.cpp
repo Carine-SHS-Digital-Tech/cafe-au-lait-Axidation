8d99e571f9bd5f47e2a42989e879680c088f8140
CallanIsTaken <zoomatron10@gmail.com>
2021-08-08 21:42:47 +0800

    Functional
    All the basic functions are working

--- cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
+++ cafe-au-lait-Axidation/cafe-au-lait-Axidation.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 using namespace std;
+
+int T_DineIn = 0;
+int T_TakeOut = 0;
+int T_Order = 0;
+int T_NumCappuccino = 0;
+int T_NumEspresso = 0;
+int T_NumLatte = 0;
+int T_NumIcedCoffee = 0;
+int T_Cups = 0;
+float T_TotalGST = 0;
+float T_TotalPrice = 0;
+
+ofstream DailySummaryCSV("DailySummary.csv");
 
 void NewOrder()
 {
+    int DineIn = 0;
+    int TakeOut = 0;
+    int NumCappuccino = 0;
+    int NumEspresso = 0;
+    int NumLatte = 0;
+    int NumIcedCoffee = 0;
+    int Cups = 0;
+    float TotalGST = 0;
+    float TotalPrice = 0;
     float Cappuccino;
     float Espresso;
     float Latte;
     float IcedCoffee;
     float Surcharge = 0;
-    int NumCappuccino;
-    int NumEspresso;
-    int NumLatte;
-    int NumIcedCoffee;
 
     string choice2;
-    std::cout << "Dine-In(1) or Take-Away(2)?\n";
-    std::cout << "*Take-Away incurs a 5% surcharge\n";
-    std::cin >> choice2;
-    std::cout << "\n";
+    cout << "Dine-In(1) or Take-Away(2)?\n";
+    cout << "*Take-Away incurs a 5% surcharge\n";
+    cin >> choice2;
+    if (choice2 != "1" && choice2 != "2") { printf("\nAn error occured."); return; }
+    cout << "\n";
 
     if (choice2 == "1")
     {
@@ -27,6 +48,7 @@ void NewOrder()
         Latte = 2.75f;
         IcedCoffee = 2.75f;
         printf("~-MENU-~ (inc. 10%% GST)\n1 - Cappuccino $%.2f\n2 - Esspresso $%.2f\n3 - Latte $%.2f\n4 - Iced Coffee $%.2f\n\n", Cappuccino, Espresso, Latte, IcedCoffee);
+        DineIn = 1;
 
     }
 
@@ -38,19 +60,20 @@ void NewOrder()
         Latte = 2.8875;
         IcedCoffee = 2.8875;
         printf("~-MENU-~ (inc. 10%% GST + 5%% Surcharge)\n1 - Cappuccino $%.2f\n2 - Esspresso $%.2f\n3 - Latte $%.2f\n4 - Iced Coffee $%.2f\n\n", Cappuccino, Espresso, Latte, IcedCoffee);
+        TakeOut = 1;
 
     }
 
     printf("Please enter desired amount per item\n");
-    printf("1: "); std::cin >> NumCappuccino;
-    printf("2: "); std::cin >> NumEspresso;
-    printf("3: "); std::cin >> NumLatte;
-    printf("4: "); std::cin >> NumIcedCoffee;
-    std::cout << "\n";
+    printf("1: "); cin >> NumCappuccino;
+    printf("2: "); cin >> NumEspresso;
+    printf("3: "); cin >> NumLatte;
+    printf("4: "); cin >> NumIcedCoffee;
+    cout << "\n";
 
     if (choice2 == "2") Surcharge = 0.165 * NumCappuccino + 0.12375 * NumEspresso + 0.1375 * NumLatte + 0.1375 * NumIcedCoffee;
 
-    float TotalPrice = NumCappuccino * Cappuccino + NumEspresso * Espresso + NumLatte * Latte + NumIcedCoffee * IcedCoffee;
+    TotalPrice = NumCappuccino * Cappuccino + NumEspresso * Espresso + NumLatte * Latte + NumIcedCoffee * IcedCoffee;
 
     printf("| Quantity | Item        | Single Item Ex. GST | GST         |  Total  |\n");
     printf("| %-9.d| Cappuccino  | $3.00               | $0.30       | $%-6.2f |\n", NumCappuccino, NumCappuccino * Cappuccino);
@@ -61,9 +84,10 @@ void NewOrder()
 
     float Tendered;
     float Change;
-    printf("Amount Tendered ($): "); std::cin >> Tendered; std::cout << "\n\n\n";
+    printf("Amount Tendered ($): "); cin >> Tendered; cout << "\n\n\n";
     Change = Tendered - TotalPrice;
 
+    printf("SALES RECEIPT -\n");
     printf("_____---------------*****~~~| Cafe au Lait |~~~*****---------------_____\n");
     printf("| Quantity | Item        | Single Item Ex. GST | GST         |  Total  |\n");
     printf("| %-9.1d| Cappuccino  | $3.00               | $0.30       | $%-6.2f |\n", NumCappuccino, NumCappuccino * Cappuccino);
@@ -73,38 +97,77 @@ void NewOrder()
     printf(" Surcharge : $%-6.2f Overall Total : $%-6.2f\n", Surcharge, TotalPrice);
     printf(" Tendered : $%-6.2f Change : $%-6.2f\n\n", Tendered, Change);
 
-    float TotalGST = 0.3 * NumCappuccino + 0.225 * NumEspresso + 0.25 * NumLatte + 0.25 * NumIcedCoffee;
+    TotalGST = 0.3 * NumCappuccino + 0.225 * NumEspresso + 0.25 * NumLatte + 0.25 * NumIcedCoffee;
+    Cups = NumCappuccino + NumEspresso + NumLatte + NumIcedCoffee;
+
+    T_DineIn += DineIn;
+    T_TakeOut += TakeOut;
+    T_Order += 1;
+    T_NumCappuccino += NumCappuccino;
+    T_NumEspresso += NumEspresso;
+    T_NumLatte += NumLatte;
+    T_NumIcedCoffee += NumIcedCoffee;
+    T_Cups = T_Cups + Cups;
+    T_TotalGST += TotalGST;
+    T_TotalPrice += (TotalPrice - TotalGST);
+
+    return;
 
 };
 
-void program()
+void DailySummary()
 {
+    printf("\nTotal Dine-In    |  %d", T_DineIn);
+    printf("\nTotal Take-Away  |  %d", T_TakeOut);
+    printf("\nTotal Orders     |  %d", T_Order);
+    printf("\nTotal Cappuccino |  %d", T_NumCappuccino);
+    printf("\nTotal Espresso   |  %d", T_NumEspresso);
+    printf("\nTotal Latte      |  %d", T_NumLatte);
+    printf("\nTotal Iced Coffee|  %d", T_NumIcedCoffee);
+    printf("\nTotal Cups Sold  |  %d", T_Cups);
+    printf("\nTotal GST        | $%.2f", T_TotalGST);
+    printf("\nTotal Income     | $%.2f\n\n", T_TotalPrice);
 
-    string choice1;
-    std::cout << "New order(1) or Daily Summary(2)?\n";
-    std::cin >> choice1;
-    std::cout << "\n";
+    DailySummaryCSV << "Total Dine-In, " << setprecision(2) << fixed << T_DineIn;
+    DailySummaryCSV << "\nTotal Take-Away, " << setprecision(2) << fixed << T_TakeOut;
+    DailySummaryCSV << "\nTotal Orders, " << setprecision(2) << fixed << T_Order;
+    DailySummaryCSV << "\nTotal Cappuccino, " << setprecision(2) << fixed << T_NumCappuccino;
+    DailySummaryCSV << "\nTotal Espresso, " << setprecision(2) << fixed << T_NumEspresso;
+    DailySummaryCSV << "\nTotal Latte, " << setprecision(2) << fixed << T_NumLatte;
+    DailySummaryCSV << "\nTotal Iced Coffee, " << setprecision(2) << fixed << T_NumIcedCoffee;
+    DailySummaryCSV << "\nTotal Cups Sold, " << setprecision(2) << fixed << T_Cups;
+    DailySummaryCSV << "\nTotal GST, $" << setprecision(2) << fixed << T_TotalGST;
+    DailySummaryCSV << "\nTotal Income, $" << setprecision(2) << fixed << T_TotalPrice;
 
+    return;
+}
 
-    if (choice1 == "1") NewOrder();
-    //if (choice1 == "1") DailySummary();
-
-};
-
-int main()
+void main()
 {
 
     string restart;
+    string choice1;
+
+    int Orders = 0; int DineIn = 0; int TakeOut = 0; int TotalCups = 0; float NumCappuccino = 0;
+    float NumEspresso = 0; float NumLatte = 0; float NumIcedCoffee = 0; float TotalGST = 0; float TotalPrice = 0;
 
     while (restart != "n")
     {
 
-        program();
+        cout << "New order(1) or Daily Summary(2)?\n";
+        cin >> choice1;
+        cout << "\n";
 
-        printf("\nRestart program? (y/n)\n");
+        if (choice1 == "1") NewOrder();
+        if (choice1 == "2") DailySummary();
+        if (choice1 != "1" && choice1 != "2") { printf("\nAn error occured."); return; }
+
+        printf("\nBack to Start? (y/n)\n");
         cin >> restart;
         printf("\n");
 
     }
+
+    return;
 
 };
